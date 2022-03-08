91759823093238cf8c6c705680d8fd655ae09ebf
rosemarypalackal1991 <rosemarypalackal1991@gmail.com>
2022-03-08 13:04:11 +0530

    var assignment

---
+++ var_assignment.py
@@ -0,0 +1,14 @@
+
+# Variable assignment in python is dynamically typed 
+#as against other programming languages which are statically typed
+# a =2, Later on in my program, i can give a = ("cats")
+# python will accept it as we do not assign any data types along with the variable names (eg, int a=2)
+
+
+Rent = 20000
+Maintenance = 3000
+Maintenance = 3250.25
+
+Final_rent = print(Rent + Maintenance)
+
+
