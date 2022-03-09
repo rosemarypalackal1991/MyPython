fc481922f2b5f4a39ecf6d7657c731fe763020db
rosemarypalackal1991 <rosemarypalackal1991@gmail.com>
2022-03-09 13:47:34 +0530

    added string reversing

--- 3_strings.py
+++ 3_strings.py
@@ -10,6 +10,7 @@
 
 str1 ="Hello"
 str2 ='Hello World'  #white spaces are counted as characters
+str3 = 'abcdefgh'
 
 print(str1[2])    #l
 print(str2[5])    #space
@@ -17,6 +18,16 @@ print(str1[-2])   # l
 print(str2[1:7])  #ello W
 print(str2[1:7:2])  #el     
 print(str1[-4:-1])       #ell
+print(str1[1:]) #ello  (till the end)
+
+print(str3[3:6])  #def
+print(str3[1:3])  #bc
+print(str3[::2]) #aceg - beginnig to end in step of 2
+
+
+##########REVERSE A STRING################
+print(str3[::-1])  #hgfedcba
+
 
 # Escape sequence
 
